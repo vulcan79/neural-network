094dfa79f86d87c9c71435c314703481ec84686a
Vulcan79 <steen.erik@telia.com>
2022-07-06 23:19:10 +0200

    First commit

---
+++ neuralnetwork1/src/main/java/erstee/neuralnetwork1/App.java
@@ -0,0 +1,58 @@
+package erstee.neuralnetwork1;
+//@formatter:off
+/*
+ * INOUT	AND	OR	XOR	NOR	NAND	XNOR
+ * 00		0	0	0	1	1		1
+ * 01		0	1	1	0	1		0
+ * 10		0	1	1	0	1		0
+ * 11		1	1	0	0	0		1
+ */
+//@formatter:on
+
+public class App {
+	static double neuron(double[] x, double[] w, double b) {
+		// Weighted sum
+		double z = 0.0;
+
+		for (int i = 0; i < x.length; i++) {
+			z += x[i] * w[i];
+		}
+		z += b;
+
+		// activation
+		return z > 0 ? 1.0 : 0.0;
+	}
+
+	static double and(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
+	}
+
+	static double or(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, 0);
+	}
+
+	static double xor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
+	}
+
+	static double nor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { -1, -1 }, 1);
+	}
+
+	static double nand(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { -1, -1 }, 2);
+	}
+
+	static double xnor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {
+			double x1 = i / 2;
+			double x2 = i % 2;
+			double output = or(x1, x2);
+			System.out.printf("%d%d\t%d\n", (int) x1, (int) x2, (int) output);
+		}
+	}
+}
